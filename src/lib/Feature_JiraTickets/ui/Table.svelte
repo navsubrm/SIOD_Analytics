64f97413edea7e337ce87bb97667399ec77b77da
Nathan M <70189952+navsubrm@users.noreply.github.com>
2025-04-16 08:45:18 -0600

    Update Table.svelte
    Update to fix current stage display.

--- src/lib/Feature_JiraTickets/ui/Table.svelte
+++ src/lib/Feature_JiraTickets/ui/Table.svelte
@@ -23,8 +23,8 @@
 
 	function getCurrentRelease(item) {
 		const released = item?.releaseStages.filter((el: ReleaseStage) => el.date !== null);
-		const sorted = released.sort((a: ReleaseStage, b: ReleaseStage) => a.date < b.date);
-		return sorted[0];
+		//const sorted = released.sort((a: ReleaseStage, b: ReleaseStage) => a.date < b.date);
+		return released[released.length - 1];
 	}
 
 	//Update List from other features with update list store.
